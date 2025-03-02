aa3f88be4a68a0dfd140e03ed7f914386e347637
umeshyadav88281 <umesh88281@gmail.com>
2025-03-01 22:00:33 -0500

    Function to update the background color

--- lab-3/js/page-color-changer.js
+++ lab-3/js/page-color-changer.js
@@ -3,3 +3,12 @@ const redSlider = document.getElementById('redSlider');
 const greenSlider = document.getElementById('greenSlider');
 const blueSlider = document.getElementById('blueSlider');
 const pageBody = document.body;
+
+// Function to update the background color
+function updateBackgroundColor() {
+    const red = redSlider.value;
+    const green = greenSlider.value;
+    const blue = blueSlider.value;
+    const rgbColor = `rgb(${red}, ${green}, ${blue})`;
+    pageBody.style.backgroundColor = rgbColor;
+}
