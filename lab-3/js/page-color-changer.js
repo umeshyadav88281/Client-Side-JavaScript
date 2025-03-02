976f367533dd03e4e1bf65d9e225c786aa7d9b75
umeshyadav88281 <umesh88281@gmail.com>
2025-03-01 21:56:02 -0500

    Declare and initialize constants

---
+++ lab-3/js/page-color-changer.js
@@ -0,0 +1,5 @@
+// Declare and initialize constants for the input slider elements and the page body
+const redSlider = document.getElementById('redSlider');
+const greenSlider = document.getElementById('greenSlider');
+const blueSlider = document.getElementById('blueSlider');
+const pageBody = document.body;
